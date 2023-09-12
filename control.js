b2a8bcb849f02420e1c22d420600a12c0599a6e7
Usama Rasheed <65440307+usama2188@users.noreply.github.com>
2023-09-12 17:51:34 +0500

    control structure

---
+++ control.js
@@ -0,0 +1,139 @@
+//if else
+
+let num = 2;
+
+// if (num > 0) {
+//     console.log(num + ' is greater than zero')
+// }
+// else if (num < 0) {
+//     console.log(num + ' is smaller than zero')
+// } 
+// else {
+//     console.log(num)
+// }
+
+// Switch
+
+// switch (num) {
+//     case num > 0:
+//         console.log(num + ' is greater than zero')
+//         break;
+//     case num < 0:
+//         console.log(num + ' is greater than zero')
+//         break;
+
+//     default:
+//         console.log(num)
+// }
+
+
+// var albhabet = 'm';
+
+// switch (albhabet) {
+//     case 'a':
+//         console.log("Alphabet is a");
+//         break;
+//     case 'b':
+//         console.log("Alphabet is b");
+//         break;
+//     case 'c':
+//         console.log("Alphabet is c");
+//         break;
+
+
+//     default:
+//         console.log("ANY other");
+// }
+
+
+
+//Loops in js
+// while loop
+// do while
+// for loop
+
+
+// let i=10;
+// while(i>0){
+//     console.log(i)
+//     i--
+// }
+
+
+// let j = 20
+// do {
+//     console.log(j)
+//     j++
+// } while (j <= 10)
+
+
+// for(let k=0;k<=10;k++){
+//     console.log(k)
+// }
+
+
+let obj={
+    name:'usama',
+    address:"kjlkjkljklj",
+    phone:'32434244',
+    age:23,
+    education:'graduation'
+}
+
+// [
+//     name:'usama',
+//     address:"kjlkjkljklj"   
+// ]
+
+
+// FOR IN LOOP
+// for(let key in obj){
+//     console.log(obj[key])
+// }
+
+
+
+let arr=[
+    {
+        name:'a',
+        address:"kjlkjkljklj",
+        phone:'32434244',
+        age:23,
+        education:'graduation'
+    },{
+        name:'b',
+        address:"kjlkjkljklj",
+        phone:'32434244',
+        age:23,
+        education:'graduation'
+    }
+    ,{
+        name:'c',
+        address:"kjlkjkljklj",
+        phone:'32434244',
+        age:23,
+        education:'graduation'
+    }
+]
+
+
+
+// FOR OF LOOP
+
+// let arr=[11,22,33,44,55,66,77]
+
+
+// for(let x of arr){
+//     console.log(x)
+// }
+
+
+
+//ARRAY METHOD
+// arr.map((x)=>{
+// console.log(x)
+// })
+
+
+//Array method
+// arr
